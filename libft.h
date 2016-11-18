6ee9c8122fc2ac64a48dcde469b120373ad53b00
tiskow <tybbow@hotmail.fr>
2016-11-18 00:45:29 -0800

    push libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tiskow <tiskow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/04 14:20:06 by tiskow            #+#    #+#             */
-/*   Updated: 2016/11/16 08:14:47 by tiskow           ###   ########.fr       */
+/*   Updated: 2016/11/17 05:42:26 by tiskow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
+size_t	ft_strclen(char const *str, char c);
 size_t	ft_strlen(const char *str);
 size_t	ft_intlen(int n);
-size_t	ft_dstrlen(char **str);
 char	*ft_strdup(const char *str);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
