6606869e32b61c7e54b181448b957b46a4d419a5
Tony ISKOW <tiskow@e3r12p11.42.fr>
2016-12-17 15:14:01 +0100

    fix error lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: tiskow <tiskow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/13 07:00:32 by tiskow            #+#    #+#             */
-/*   Updated: 2016/11/21 01:14:15 by tiskow           ###   ########.fr       */
+/*   Updated: 2016/12/17 15:13:19 by tiskow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	if (*alst && del)
-	{
+	if ((*alst)->next != NULL)
 		ft_lstdel(&(*alst)->next, del);
-		ft_lstdelone(alst, del);
-	}
+	ft_lstdelone(alst, del);
 }
